c8b866faf9a1d226d9ac5f922e28076c11c41c3f
Nikita <jrringler13759@gmail.com>
2020-08-04 18:01:54 -0400

    added build folder for deployment

---
+++ build/precache-manifest.226f83e0a696927a924f51cbbd849ee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38df6530457bcd8205d8c9a3cf62d04",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "dce873640e7cad8542f0",
+    "url": "/reactportfolio/static/css/main.731b26a2.chunk.css"
+  },
+  {
+    "revision": "e966b284d444246e5823",
+    "url": "/reactportfolio/static/js/2.1567b78c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactportfolio/static/js/2.1567b78c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce873640e7cad8542f0",
+    "url": "/reactportfolio/static/js/main.995db791.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  }
+]);
